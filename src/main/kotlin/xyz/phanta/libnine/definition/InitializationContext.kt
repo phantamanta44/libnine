c9be4f37a4764e03891bdf895b8c145c175ca97b
phantamanta44 <evandalong@gmail.com>
2019-03-03 17:47:18 -0600

    Event registration on init contexts

--- src/main/kotlin/xyz/phanta/libnine/definition/InitializationContext.kt
+++ src/main/kotlin/xyz/phanta/libnine/definition/InitializationContext.kt
@@ -1,14 +1,23 @@
 package xyz.phanta.libnine.definition
 
+import net.minecraftforge.eventbus.api.Event
+import net.minecraftforge.eventbus.api.EventPriority
 import net.minecraftforge.eventbus.api.IEventBus
 import xyz.phanta.libnine.Virtue
 
-class InitializationContext(mod: Virtue, eventBus: IEventBus) {
+class InitializationContext(mod: Virtue, val eventBus: IEventBus) {
 
     private val registrar: Registrar = Registrar(mod, eventBus)
 
     fun define(vararg definers: Definer) {
         DefinitionDefContext(registrar).let { ctx -> definers.forEach { it.definitions()(ctx) } }
+    }
+
+    inline fun <reified E : Event> listen(
+            priority: EventPriority = EventPriority.NORMAL,
+            receiveCancelled: Boolean = false,
+            noinline listener: (E) -> Unit) {
+        eventBus.addListener(priority, receiveCancelled, E::class.java, listener)
     }
 
 }
