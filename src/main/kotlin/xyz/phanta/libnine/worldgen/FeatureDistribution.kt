89ebcd3bcee22b5d3e315afcb463bae2bba90c23
phantamanta44 <evandalong@gmail.com>
2019-03-09 23:42:41 -0600

    Uniform feature distribution

--- src/main/kotlin/xyz/phanta/libnine/worldgen/FeatureDistribution.kt
+++ src/main/kotlin/xyz/phanta/libnine/worldgen/FeatureDistribution.kt
@@ -8,6 +8,19 @@ import xyz.phanta.libnine.util.math.clamp
 import java.util.*
 import kotlin.math.floor
 
+class FeatureDistributionUniform(private val count: Int, private val minHeight: Int, private val variation: Int) : NineFeatureDistribution() {
+
+    override fun computeDistribution(
+            world: IWorld,
+            chunkGen: IChunkGenerator<out IChunkGenSettings>,
+            origin: BlockPos,
+            rand: Random
+    ): List<BlockPos> = (1..count).map {
+        origin.add(rand.nextInt(16), minHeight + rand.nextInt(variation + 1), rand.nextInt(16))
+    }
+
+}
+
 class FeatureDistributionNormal(private val count: Int, private val mean: Double, private val variation: Double) : NineFeatureDistribution() {
 
     override fun computeDistribution(
