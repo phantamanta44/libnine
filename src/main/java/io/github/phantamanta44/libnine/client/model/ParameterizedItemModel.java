c61d929b5a0a6b508c806a79fec83fc5527ed888
phantamanta44 <evandalong@gmail.com>
2019-09-16 20:38:11 -0500

    Optimize parametric item model mutation resolution

--- src/main/java/io/github/phantamanta44/libnine/client/model/ParameterizedItemModel.java
+++ src/main/java/io/github/phantamanta44/libnine/client/model/ParameterizedItemModel.java
@@ -1,12 +1,13 @@
 package io.github.phantamanta44.libnine.client.model;
 
 import com.google.common.collect.Table;
+import com.google.common.collect.Tables;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
-import gnu.trove.map.TObjectIntMap;
-import gnu.trove.map.hash.TObjectIntHashMap;
+import io.github.phantamanta44.libnine.util.ImpossibilityRealizedException;
 import io.github.phantamanta44.libnine.util.helper.JsonUtils9;
+import io.github.phantamanta44.libnine.util.tuple.IPair;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.client.renderer.block.model.IBakedModel;
@@ -26,41 +27,69 @@ import javax.annotation.Nullable;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class ParameterizedItemModel implements IModel {
 
-    private final TObjectIntMap<String> indexMap;
-    private final Map<Mutation, IModel> children;
+    private final Map<String, IPair<Integer, String[]>> mutKeyMap;
+    private final List<IPair<String, String[]>> mutKeyMapInv;
+    private final MutationState[] mutationSet;
+    private final IModel[] models;
 
     ParameterizedItemModel(ParameterizedItemModelLoader.ResourceInjector resourceInjector,
                            JsonObject archetype, @Nullable Table<String, String, JsonElement> mutations) {
-        this.indexMap = new TObjectIntHashMap<>();
+        this.mutKeyMap = new HashMap<>();
+        this.mutKeyMapInv = new ArrayList<>();
         if (mutations != null) {
-            String[] keys = new String[mutations.rowKeySet().size()];
+            int[] maxValueIndices = new int[mutations.rowKeySet().size()];
+            int totalMutCount = 1;
             int index = 0;
-            for (String key : mutations.rowKeySet()) {
-                this.indexMap.put(key, index);
-                keys[index] = key;
-                index++;
+            for (Map.Entry<String, Map<String, JsonElement>> keyEntry : mutations.rowMap().entrySet()) {
+                Map<String, JsonElement> valueMap = keyEntry.getValue();
+                String key = keyEntry.getKey();
+                String[] valueSet = valueMap.keySet().toArray(new String[0]);
+                mutKeyMap.put(key, IPair.of(index, valueSet));
+                mutKeyMapInv.add(IPair.of(key, valueSet));
+                int valueCount = valueMap.size();
+                maxValueIndices[index] = valueCount - 1;
+                totalMutCount *= valueCount;
+                ++index;
             }
-            this.children = calculateCartesianProduct(keys, mutations, 0)
-                    .collect(Collectors.toMap(m -> m, m -> mutateModel(archetype, mutations, m).compile(resourceInjector)));
+            this.mutationSet = new MutationState[totalMutCount];
+            this.models = new IModel[totalMutCount];
+            int[] valueIndices = new int[maxValueIndices.length];
+            index = 0;
+            outer:
+            while (true) {
+                int[] currentValueIndices = Arrays.copyOf(valueIndices, valueIndices.length);
+                MutationState mut = new MutationState(this, index, currentValueIndices);
+                mutationSet[index] = mut;
+                for (int i = 0; i < valueIndices.length; i++) {
+                    if (++valueIndices[i] > maxValueIndices[i]) {
+                        if (i == valueIndices.length - 1) {
+                            break outer;
+                        }
+                        valueIndices[i] = 0;
+                    } else {
+                        break;
+                    }
+                }
+                ++index;
+            }
+            for (MutationState mut : mutationSet) {
+                mut.cacheAdjacents();
+                models[mut.index] = mutateModel(archetype, mutations, mut).compile(resourceInjector);
+            }
         } else {
-            this.children = Collections.singletonMap(new Mutation(this), new IChildModel.JsonModel(archetype).compile(resourceInjector));
+            this.mutationSet = new MutationState[] { new MutationState(this, 0, new int[0]) };
+            this.models = new IModel[] { new IChildModel.JsonModel(archetype).compile(resourceInjector) };
         }
     }
 
-    private Stream<Mutation> calculateCartesianProduct(String[] keys, Table<String, String, JsonElement> mutations, int index) {
-        Stream<Mutation> prev = index == keys.length - 1
-                ? Stream.of(new Mutation(this)) : calculateCartesianProduct(keys, mutations, index + 1);
-        return prev.flatMap(m -> mutations.row(keys[index]).keySet().stream().map(v -> m.mutateCloning(keys[index], v)));
-    }
-
-    private IChildModel mutateModel(JsonObject model, Table<String, String, JsonElement> mutations, Mutation mutation) {
+    private IChildModel mutateModel(JsonObject model, Table<String, String, JsonElement> mutations, MutationState mutation) {
         JsonObject result = JsonUtils9.copy(model);
-        for (String key : indexMap.keySet()) {
-            JsonElement mutator = mutations.get(key, mutation.values[indexMap.get(key)]);
+        for (int i = 0; i < mutKeyMapInv.size(); i++) {
+            IPair<String, String[]> keyEntry = mutKeyMapInv.get(i);
+            JsonElement mutator = mutations.get(keyEntry.getA(), keyEntry.getB()[mutation.valueIndices[i]]);
             if (mutator.isJsonObject()) {
                 JsonUtils9.merge(result, mutator.getAsJsonObject());
             } else if (mutator.isJsonPrimitive()) {
@@ -89,8 +118,8 @@ public class ParameterizedItemModel implements IModel {
 
     @Override
     public IBakedModel bake(IModelState state, VertexFormat format, Function<ResourceLocation, TextureAtlasSprite> textureBaker) {
-        return new ParameterizedItemModelBakedModelDelegator(this, children.entrySet().stream()
-                .collect(Collectors.toMap(Map.Entry::getKey, e -> e.getValue().bake(state, format, textureBaker))));
+        return new ParameterizedItemModelBakedModelDelegator(this,
+                Arrays.stream(models).map(m -> m.bake(state, format, textureBaker)).toArray(IBakedModel[]::new));
     }
 
     private interface IChildModel {
@@ -141,13 +170,13 @@ public class ParameterizedItemModel implements IModel {
     private static class ParameterizedItemModelBakedModelDelegator implements IBakedModel {
 
         private final ParameterizedItemModel model;
-        private final Map<Mutation, IBakedModel> children;
+        private final IBakedModel[] children;
         private final IBakedModel defaultDelegate;
 
-        ParameterizedItemModelBakedModelDelegator(ParameterizedItemModel model, Map<Mutation, IBakedModel> children) {
+        ParameterizedItemModelBakedModelDelegator(ParameterizedItemModel model, IBakedModel[] children) {
             this.model = model;
             this.children = children;
-            this.defaultDelegate = children.values().iterator().next();
+            this.defaultDelegate = children[0];
         }
 
         @Override
@@ -184,19 +213,17 @@ public class ParameterizedItemModel implements IModel {
 
             private final ParameterizedItemModelBakedModelDelegator container;
 
-            public ParameterizedItemModelOverrideList(ParameterizedItemModelBakedModelDelegator container) {
+            ParameterizedItemModelOverrideList(ParameterizedItemModelBakedModelDelegator container) {
                 super(Collections.emptyList());
                 this.container = container;
             }
 
             @Override
-            public IBakedModel handleItemState(IBakedModel originalModel, ItemStack stack, @Nullable World world, @Nullable EntityLivingBase entity) {
-                Mutation mutation = new Mutation(container.model);
+            public IBakedModel handleItemState(IBakedModel originalModel, ItemStack stack,
+                                               @Nullable World world, @Nullable EntityLivingBase entity) {
+                Mutation mutation = new Mutation(container.model.mutationSet[0]);
                 ((IContextSensitive)stack.getItem()).getModelMutations(stack, world, entity, mutation);
-                if (!container.children.containsKey(mutation)) {
-                    throw new NoSuchElementException("Invalid PI mutation: " + mutation);
-                }
-                IBakedModel delegate = container.children.get(mutation);
+                IBakedModel delegate = container.children[mutation.state.index];
                 return delegate.getOverrides().handleItemState(delegate, stack, world, entity);
             }
 
@@ -223,51 +250,73 @@ public class ParameterizedItemModel implements IModel {
 
     public static class Mutation {
 
-        private final TObjectIntMap<String> indexMap;
-        private final String[] values;
+        private MutationState state;
 
-        public Mutation(ParameterizedItemModel model) {
-            this.indexMap = model.indexMap;
-            this.values = new String[model.indexMap.size()];
-        }
-
-        private Mutation(Mutation original) {
-            this.indexMap = original.indexMap;
-            this.values = Arrays.copyOf(original.values, original.values.length);
+        Mutation(MutationState initialState) {
+            this.state = initialState;
         }
 
         public Mutation mutate(String key, String value) {
-            values[indexMap.get(key)] = value;
+            state = state.mutate(key, value);
             return this;
         }
 
-        Mutation mutateCloning(String key, String value) {
-            return values[indexMap.get(key)] == null ? mutate(key, value) : new Mutation(this).mutate(key, value);
+    }
+
+    private static class MutationState {
+
+        private final ParameterizedItemModel model;
+        private final int index;
+        private final int[] valueIndices;
+        private final Table<String, String, MutationState> adjTable;
+
+        MutationState(ParameterizedItemModel model, int index, int[] valueIndices) {
+            this.model = model;
+            this.index = index;
+            this.valueIndices = valueIndices;
+            //noinspection UnstableApiUsage
+            this.adjTable = Tables.newCustomTable(new HashMap<>(), HashMap::new);
         }
 
-        @Override
-        public boolean equals(Object o) {
-            if (!(o instanceof Mutation)) return false;
-            for (int i = 0; i < values.length; i++) {
-                if (!((Mutation)o).values[i].equals(values[i])) return false;
+        void cacheAdjacents() {
+            model.mutKeyMap.forEach((key, keyEntry) -> {
+                int keyIndex = keyEntry.getA();
+                String[] valueSet = keyEntry.getB();
+                for (int i = 0; i < valueSet.length; i++) {
+                    int valueIndex = i; // no nonfinals in lambdas
+                    // TODO this should optimally compute the index directly
+                    adjTable.put(key, valueSet[valueIndex], Arrays.stream(model.mutationSet)
+                            .filter(mut -> arrayEqualsWithChange(valueIndices, keyIndex, valueIndex, mut.valueIndices))
+                            .findAny().orElseThrow(ImpossibilityRealizedException::new));
+                }
+            });
+        }
+
+        public MutationState mutate(String key, String value) {
+            MutationState result = adjTable.get(key, value);
+            if (result == null) {
+                throw new IllegalArgumentException("Invalid mutation: " + key + "=" + value);
             }
-            return true;
-        }
-
-        @Override
-        public int hashCode() {
-            int hash = 0;
-            for (int i = 0; i < values.length; i++) hash |= (values[i].hashCode() << (i * 4));
-            return hash;
+            return result;
         }
 
         @Override
         public String toString() {
-            return "Mutation { " + indexMap.keySet().stream()
-                    .map(k -> String.format("%s: %s", k, values[indexMap.get(k)]))
+            return "Mutation { " + model.mutKeyMap.entrySet().stream()
+                    .map(entry -> String.format("%s=%s",
+                            entry.getKey(), entry.getValue().getB()[valueIndices[entry.getValue().getA()]]))
                     .collect(Collectors.joining(", ")) + " }";
         }
 
+    }
+
+    private static boolean arrayEqualsWithChange(int[] arr, int changeIndex, int changeValue, int[] toTest) {
+        for (int i = 0; i < arr.length; i++) {
+            if (toTest[i] != (i == changeIndex ? changeValue : arr[i])) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
