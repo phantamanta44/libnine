695c5cb9464b60de017681fbc4d7539b3385b86d
phantamanta44 <evandalong@gmail.com>
2021-10-29 13:29:47 -0500

    Generalize and clean up RedstoneBehaviour

--- src/main/java/io/github/phantamanta44/libnine/util/world/RedstoneBehaviour.java
+++ src/main/java/io/github/phantamanta44/libnine/util/world/RedstoneBehaviour.java
@@ -1,38 +1,67 @@
 package io.github.phantamanta44.libnine.util.world;
 
+import com.google.common.collect.ImmutableList;
 import io.github.phantamanta44.libnine.constant.NameConst;
-import io.github.phantamanta44.libnine.util.ImpossibilityRealizedException;
 import io.github.phantamanta44.libnine.util.format.ILocalizable;
-
-import java.util.function.Predicate;
 
 public enum RedstoneBehaviour implements ILocalizable {
 
-    IGNORED(p -> true),
-    DIRECT(p -> p.getWorld().isBlockPowered(p.getPos())),
-    INVERTED(p -> !p.getWorld().isBlockPowered(p.getPos()));
+    IGNORED {
+        @Override
+        public boolean canWork(boolean powered) {
+            return true;
+        }
 
-    private final Predicate<WorldBlockPos> pred;
+        @Override
+        public boolean canWork(WorldBlockPos pos) {
+            return true;
+        }
 
-    RedstoneBehaviour(Predicate<WorldBlockPos> pred) {
-        this.pred = pred;
-    }
+        @Override
+        public String getTranslationKey() {
+            return NameConst.INFO_REDSTONE_BEHAVIOUR_IGNORED;
+        }
+    },
+    DIRECT {
+        @Override
+        public boolean canWork(boolean powered) {
+            return powered;
+        }
 
-    public boolean canWork(WorldBlockPos pos) {
-        return pred.test(pos);
-    }
+        @Override
+        public boolean canWork(WorldBlockPos pos) {
+            return pos.getWorld().isBlockPowered(pos.getPos());
+        }
+
+        @Override
+        public String getTranslationKey() {
+            return NameConst.INFO_REDSTONE_BEHAVIOUR_DIRECT;
+        }
+    },
+    INVERTED {
+        @Override
+        public boolean canWork(boolean powered) {
+            return !powered;
+        }
+
+        @Override
+        public boolean canWork(WorldBlockPos pos) {
+            return !pos.getWorld().isBlockPowered(pos.getPos());
+        }
+
+        @Override
+        public String getTranslationKey() {
+            return NameConst.INFO_REDSTONE_BEHAVIOUR_INVERTED;
+        }
+    };
+
+    public static final ImmutableList<RedstoneBehaviour> VALUES = ImmutableList.copyOf(values());
+
+    public abstract boolean canWork(boolean powered);
+
+    public abstract boolean canWork(WorldBlockPos pos);
 
     @Override
-    public String getTranslationKey() {
-        switch (this) {
-            case IGNORED:
-                return NameConst.INFO_REDSTONE_BEHAVIOUR_IGNORED;
-            case DIRECT:
-                return NameConst.INFO_REDSTONE_BEHAVIOUR_DIRECT;
-            case INVERTED:
-                return NameConst.INFO_REDSTONE_BEHAVIOUR_INVERTED;
-        }
-        throw new ImpossibilityRealizedException();
-    }
+    public abstract String getTranslationKey();
 
 }
